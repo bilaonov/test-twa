3d2ecac012e776f841bcbe4401c4267e572f1d9e
Amiran <amiran@Air-Amiran.IGD_Rostelecom>
2024-06-14 09:58:37 +0300

    change

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import '@twa-dev/sdk';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
